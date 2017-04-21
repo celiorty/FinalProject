68a56e275a80ad90d670a1394ebaf0da920af687
Tony Zhao <zzt.dt.cn@gmail.com>
2017-04-20 20:44:30 -0400

    Final Project VR Lab
    1st demo

---
+++ Assets/Code/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHeight = Input.GetAxis("Mouse Y");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveHeight, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/Code/setG.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setG : MonoBehaviour {
+
+    public float xG;
+    public float yG;
+    public float zG;
+
+    void Start () {
+        Physics.gravity = new Vector3(xG, yG, zG);
+    }
+	
+
+}
